bfa8adc274fd65536bac24779705ca1c0ec146a8
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2026-05-26 13:33:17 +0300

    delete some copm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,18 +44,17 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Collection from "./pages/Collection";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Product from "./pages/Product";
+
 import AboutMe from "./pages/AboutMe";
-import Cart from "./pages/Cart";
+
 import Login from "./pages/Login";
-import PlaceOrder from "./pages/PlaceOrder";
-import Orders from "./pages/Orders";
+// import PlaceOrder from "./pages/PlaceOrder";
+// import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import Searshbar from "./components/Searshbar";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
@@ -88,11 +87,9 @@ const App = () => {
       <div className=" ">
         <ToastContainer />
         <Navbar  />
-        {/* <Searshbar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about-me" element={<AboutMe />} />
-          {/* <Route path="/collection" element={<Collection />} /> */}
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact/>} />
           <Route path="/packages" element={<Packages />} />
@@ -100,12 +97,7 @@ const App = () => {
           <Route path="/results-for-me" element={<Resultsforme />} />
           <Route path="/important-notes" element={<ImportantNotes />} />
           <Route path="/contact-section" element={<ContactSection/>} />
-          {/* <Route path="/product/:productId" element={<Product />} /> */}
-          {/* <Route path="/cart" element={<Cart />} /> */}
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/place-order" element={<PlaceOrder />} /> */}
-          {/* <Route path="/orders" element={<Orders />} /> */}
-        </Routes>
+       </Routes>
         <Footer />
       </div>
     </div>
--- frontend/src/components/NewsLaterBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewsLaterBox = () => {
-  return (
-    <div>NewsLaterBox</div>
-  )
-}
-
-export default NewsLaterBox
